341ebc01569af1a14cd98bebe3d4afdb1d796c44
AlbericSimperl1 <alberic.simperl@student.kuleuven.be>
2025-11-25 20:54:24 +0100

    SPI

---
+++ SPI_Tx.vhd
@@ -0,0 +1,17 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity SPIhandler is
+    port (
+        clk : in std_logic;
+        rst : in std_logic;
+        sig
+    );
+end SPIhandler;
+
+architecture rtl of SPIhandler is
+
+begin
+
+end architecture;
